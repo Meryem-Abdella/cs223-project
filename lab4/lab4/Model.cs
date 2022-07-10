e7d5e26b8a7d00aa3782c210bfb8be1bb5615e08
Meryem-Abdella <meryemshoumbroo@gmail.com>
2022-07-10 14:59:01 +0300

    lab4

---
+++ lab4/lab4/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Model
+    {
+        public string num { get; set; }
+        public string count { get; set; }
+        public string inventory_num { get; set; }
+        public string price { get; set; }
+        public string object_name { get; set; }
+        public string date { get; set; }
+        public void save()
+        {
+            Console.WriteLine("Saved Succesfully");
+
+        }
+
+
+    }
+}
